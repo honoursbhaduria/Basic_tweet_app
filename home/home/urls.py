eaf2009c10ce91c893251f4a52f44e06d99a985f
honours <honoursbhadauria@gmail.com>
2025-05-08 01:03:11 +0530

    required adjustment for auth

--- home/home/urls.py
+++ home/home/urls.py
@@ -14,12 +14,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
+
+from django.contrib.auth.urls import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
      path('', include('tweets.urls')), 
     path('tweets/', include('tweets.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+LOGIN_URL = '/accounts/login'
+
+LOGIN_REDIRECT_URL= '/tweet/'
+
+LOGOUT_REDIRECT_URL= '/tweet/'
--- home/tweets/views.py
+++ home/tweets/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Tweet
 from .forms import TweetForm , UserRegistrationForm
-
+from django.contrib.auth import login
 
 # Home Page
 def index(request):
@@ -56,6 +56,12 @@ def tweet_delete(request, tweet_id):
 def register(request):
         if request.method == "POST": 
             form =   UserRegistrationForm(request.POST)
+            if  form.is_valid():
+                user  = form.save(commit =False)
+                user.set_password(form.cleaned_data["password1"])
+                user.save()
+                login(request , user)
+                return  redirect('tweet_list')
         else:
             form = UserRegistrationForm()
         
